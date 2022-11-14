dd6bc3620de0865e0da38e1774cd01d37f8ea520
Anti-Gen <putrategu@gmail.com>
2022-11-14 12:34:14 +0700

    Project-Account-Service-App

---
+++ config/mysqlDriver.go
@@ -0,0 +1,25 @@
+package config
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+)
+
+func Connection() *sql.DB {
+	var connectionString = os.Getenv("DB_CONNECTION")
+	db, err := sql.Open("mysql", connectionString)
+	if err != nil {
+		log.Fatal("error open connection", err.Error())
+	}
+
+	errPing := db.Ping()
+	if errPing != nil {
+		log.Fatal("Error connect to db", errPing.Error())
+	} else {
+		fmt.Println("Koneksi berhasil")
+	}
+	return db
+
+}
---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"be13/project/config"
+
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+
+	dbConnection := config.Connection()
+
+	fmt.Println("Menu:")
+	fmt.Println("Masukan pilihan anda:")
+	var pilihan int
+	fmt.Scanln(&pilihan)
+
+	switch pilihan {
+	case 1:
+
+	case 2:
+
+	case 3:
+
+	case 4:
+
+	case 5:
+
+	}
+
+	defer dbConnection.Close()
+
+}
